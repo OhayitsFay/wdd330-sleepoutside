91d849210238dddac63430fb9ccd5ff5c3fd5cee
OhayitsFay <atu23001@byui.edu>
2025-07-11 06:43:03 -0500

    product detail page

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,10 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  //get current products in cart
-  let products = getLocalStorage("so-cart");
+const productId = getParam("product");
 
-  //confirm if cart is empty
-  if (products) {
-    let newProductList = products.concat(product);
-    setLocalStorage("so-cart", newProductList);
-  } else {
-    /**
-     * so-cart was empty, so we need to create a new cart
-     * initializing the cart with the product should be an array
-     */
-    setLocalStorage("so-cart", Array(product));
-  }
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
