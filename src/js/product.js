5f27441eafbcc8fa029918465820c627aa50f92f
Adedeji234501 <adedejiazeez1k@gmail.com>
2025-07-11 11:50:10 +0100

    cart fix

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let products = getLocalStorage("so-cart");
+  if (products) {
+    let productList = product.concat(product);
+    setLocalStorage("so-cart", productList);
+  } else {
+    setLocalStorage("so-cart", Array(product));
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
