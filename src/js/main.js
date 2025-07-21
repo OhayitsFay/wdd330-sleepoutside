af1da177812e6ce1f064f1317df7f4bf10ee39ea
Adedeji234501 <adedejiazeez1k@gmail.com>
2025-07-21 02:32:04 +0100

    dynamic header and footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import ProductList from "./ProductList.mjs";
 import ProductData from "./ProductData.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter("/partial/header.html", "/partial/footer.html");
 
 const data = new ProductData("tents");
 
